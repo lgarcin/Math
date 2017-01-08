54192e5272630f648be24eb0e51087718928f2d0
lgarcin <laurent.garcin@gmail.com>
2017-01-08 11:29:00 +0100

    Feigenbaum

--- Posts/feigenbaum.py
+++ Posts/feigenbaum.py
@@ -1,27 +1,27 @@
 from numpy import linspace
-from matplotlib.pyplot import scatter, show
+from matplotlib.pyplot import scatter, show, xlim, ylim
 
 
 def f(r, x):
     return r * x * (1 - x)
 
 
-def feigenbaum(r1, r2, nb):
+def feigenbaum(r1, r2, nb, N):
     r_range = linspace(r1, r2, nb)
     x = []
     y = []
     for r in r_range:
         u = .1
-        for _ in range(100):
+        for _ in range(N):
             u = f(r, u)
-        for _ in range(100):
+        for _ in range(N):
             u = f(r, u)
             x.append(r)
             y.append(u)
-    scatter(x, y, marker='.')
+    scatter(x, y, marker='.', s=1)
+    xlim(r1, r2)
+    ylim(0, 1)
     show()
 
 
-feigenbaum(3.5, 3.7, 1000)
-
-# Parler de la nature fractale
+feigenbaum(3.82, 3.86, 10000, 500)
---
+++ test.py
@@ -0,0 +1,3 @@
+from mayavi import mlab
+
+mlab.test_points3d()
